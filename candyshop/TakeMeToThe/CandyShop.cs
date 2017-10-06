5e0317125160f7fdb7e84ebb523962c66b53bc9b
bramble100 <arnold.barna@gmail.com>
2017-10-06 11:10:00 +0200

    new(CandyShop): class created

---
+++ candyshop/TakeMeToThe/CandyShop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TakeMeToThe
+{
+    class CandyShop
+    {
+        private int sugarInventory;
+
+        public CandyShop(int sugar)
+        {
+            this.sugarInventory = sugar;
+        }
+    }
+}
