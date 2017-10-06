a4539cbd01086160b2902b6f1b68267191219a32
bramble100 <arnold.barna@gmail.com>
2017-10-06 13:18:52 +0200

    improve(CandyShop): some XML documentation added

--- candyshop/TakeMeToThe/CandyShop.cs
+++ candyshop/TakeMeToThe/CandyShop.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace TakeMeToThe
 {
+    /// <summary>
+    /// Holds all the data and methods for running a nice candy shop to where you can go with Olivia or 50 Cent (or with both).
+    /// </summary>
     class CandyShop
     {
         internal static readonly Candy CANDY = new Candy();
@@ -25,6 +28,10 @@ namespace TakeMeToThe
             {typeof(Lollipop), 10m }
         };
 
+        /// <summary>
+        /// 50 Cent comes and build this for us.
+        /// </summary>
+        /// <param name="sugar"></param>
         public CandyShop(int sugar)
         {
             // The constructor should take the amount of sugar in gramms.
@@ -32,11 +39,15 @@ namespace TakeMeToThe
             Console.WriteLine("CandyShop created.");
         }
 
+        /// <summary>
+        /// Olivia brings us some provided we have the money.
+        /// </summary>
+        /// <param name="sugar"></param>
         internal void BuySugar(int sugar)
         {
             // We can buy sugar with a given number as amount.
             // If we buy sugar we can raise the CandyShop's amount of sugar and reduce the income by the price of it.
-            if(money >= sugar * sugarPrice)
+            if (money >= sugar * sugarPrice)
             {
                 sugarInventory += sugar;
                 money -= sugar * sugarPrice;
@@ -48,6 +59,10 @@ namespace TakeMeToThe
             }
         }
 
+        /// <summary>
+        /// Sweeet dreams are made of these ...
+        /// </summary>
+        /// <param name="sweet"></param>
         internal void CreateSweets(Sweet sweet)
         {
             // If we create a candie or lollipop the CandyShop's sugar amount gets reduced by the amount needed to create the sweets
@@ -68,11 +83,19 @@ namespace TakeMeToThe
             }
         }
 
+        /// <summary>
+        /// Just to know where we are.
+        /// </summary>
         internal void PrintInfo()
         {
             Console.WriteLine(ToString());
         }
 
+        /// <summary>
+        /// You should definitely ask for more than fifty cents.
+        /// </summary>
+        /// <param name="sweet"></param>
+        /// <param name="quantity"></param>
         internal void Sell(Sweet sweet, int quantity)
         {
             // We can sell candie or lollipop with a given number as amount
@@ -83,23 +106,30 @@ namespace TakeMeToThe
                 if (inventoryOfSweets[sweet.GetType()] >= quantity)
                 {
                     inventoryOfSweets[sweet.GetType()] -= quantity;
-                    money += prices[sweet.GetType()]* quantity;
+                    money += prices[sweet.GetType()] * quantity;
 
-                    Console.WriteLine($"{quantity} pcs of {sweet.GetType().Name} sold for {prices[sweet.GetType()]*quantity}");
-
+                    Console.WriteLine($"{quantity} pcs of {sweet.GetType().Name} sold for {prices[sweet.GetType()] * quantity}");
                 }
             }
         }
 
+        /// <summary>
+        /// If Olivia needs another diamond necklace, we need a little more money.
+        /// </summary>
+        /// <param name="raisePercentage"></param>
         internal void Raise(decimal raisePercentage)
         {
             // We can raise the prices of all candies and lollipops with a given percentage
 
-            prices[typeof(Candy)] *= (100 + raisePercentage)/100;
-            prices[typeof(Lollipop)] *= (100 + raisePercentage)/100;
+            prices[typeof(Candy)] *= (100 + raisePercentage) / 100;
+            prices[typeof(Lollipop)] *= (100 + raisePercentage) / 100;
             Console.WriteLine($"The prices are raised by {raisePercentage}% (new prices are: Candy: {prices[typeof(Candy)]} and Lollipop: {prices[typeof(Lollipop)]})");
         }
 
+        /// <summary>
+        /// If the boss asks ... oh it is our own shop ... whatever ... let's see what we have.
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
             // The CandyShop should print properties represented as string in this format:
@@ -107,7 +137,7 @@ namespace TakeMeToThe
 
             return $"Inventory:"
                 + $" {(inventoryOfSweets.ContainsKey(typeof(Candy)) ? inventoryOfSweets[typeof(Candy)] : 0)} candies"
-                + $" {(inventoryOfSweets.ContainsKey(typeof(Lollipop)) ? inventoryOfSweets[typeof(Lollipop)]: 0)} lollipops"
+                + $" {(inventoryOfSweets.ContainsKey(typeof(Lollipop)) ? inventoryOfSweets[typeof(Lollipop)] : 0)} lollipops"
                 + $", Income {money}, Sugar: {sugarInventory}gr";
         }
     }
