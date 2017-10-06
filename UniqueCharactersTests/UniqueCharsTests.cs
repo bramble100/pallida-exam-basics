b6bddb6afafe5c95dc480ed40e5e5d736b86dabe
bramble100 <arnold.barna@gmail.com>
2017-10-06 10:01:44 +0200

    new: unittest created; method handles empty string

---
+++ UniqueCharactersTests/UniqueCharsTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniqueChars;
+using NUnit.Framework;
+
+namespace UniqueCharactersTests
+{
+    [TestFixture]
+    public class UniqueCharsTests
+    {
+        [Test]
+        public void EmptyInputString()
+        {
+
+            string input = "hello";
+            List<string> expectedResult = new List<string>() { "h", "e", "o" };
+            List<string> receivedResult = UniqueChars.UniqueChars.UniqueCharacters(input);
+            Assert.AreEqual(receivedResult, expectedResult);
+        }
+    }
+}
---
+++ uniquechars/UniqueChars/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UniqueChars
+{
+    public class UniqueChars
+    {
+        public static void Main(string[] args)
+        {
+            // Create a method called `UniqueCharacters` that takes a string as parameter
+            // and returns a list with the unique letters of the given string
+            // Create basic unit tests for it with at least 3 different test cases
+            // Print the characters from that list in the following format:
+            // "n", "g", "r", "m"
+
+            List<string> result = UniqueCharacters("hello");
+            result.ForEach(s => Console.WriteLine(Convert.ToString(s)));
+            
+            Console.ReadKey();
+        }
+
+        public static List<string> UniqueCharacters(string inputString)
+        {
+            List<string> result = new List<string>();
+            if (String.IsNullOrEmpty(inputString))
+            {
+                return result;
+            }
+            return result;
+        }
+    }
+}
